6aed6a7d305d9e32dfddc69418baab0537f7dd90
Rabiu Kabir <reachrabiukabir@gmail.com>
2023-06-30 18:00:12 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+import Tours from "./components/Tours";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
